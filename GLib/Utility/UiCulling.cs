1a6d5ffc29a99b0732e3e6ecc14fe625aa2347de
chirp <72366111+chirpxiv@users.noreply.github.com>
2023-10-16 21:28:53 +0100

    Add discard overloads for `UiCulling.Scroll`

--- GLib/Utility/UiCulling.cs
+++ GLib/Utility/UiCulling.cs
@@ -12,11 +12,20 @@ public static class UiCulling {
 	) {
 		maxScroll = itemHeight * count;
 		scroll = Math.Min(scroll, maxScroll);
-		
+
 		start = (int)Math.Floor(scroll / itemHeight);
 		var displayCt = (int)Math.Floor(frameHeight / itemHeight);
 		displayCt = Math.Min(displayCt, count - start);
-		
+
 		return enumerator.Skip(start).Take(displayCt + 1);
 	}
+
+	public static IEnumerable<T> Scroll<T>(IEnumerable<T> enumerator, int count, float itemHeight, float frameHeight, ref float scroll)
+		=> Scroll(enumerator, count, itemHeight, frameHeight, ref scroll, out _, out _);
+	
+	public static IEnumerable<T> Scroll<T>(IEnumerable<T> enumerator, int count, float itemHeight, float frameHeight, ref float scroll, out float maxScroll)
+		=> Scroll(enumerator, count, itemHeight, frameHeight, ref scroll, out maxScroll, out _);
+	
+	public static IEnumerable<T> Scroll<T>(IEnumerable<T> enumerator, int count, float itemHeight, float frameHeight, ref float scroll, out int start)
+		=> Scroll(enumerator, count, itemHeight, frameHeight, ref scroll, out _, out start);
 }
